d8ca40ff6a2a8889c088822d53f2cec02ff9a3f0
Benjamin Christoffersen <boennecd@gmail.com>
2022-02-07 14:24:36 +0100

    finish the implementation for all branches of `mmcif_log_Lik`

--- src/mmcif-logLik.cpp
+++ src/mmcif-logLik.cpp
@@ -94,10 +94,10 @@ double mmcif_log_Lik_both_obs
    simple_mem_stack<double> &mem, ghq_data const &dat){
   mmcif_comp_helper helper{indexer, par, mem};
 
-  double lp_traject[]{helper.comp_lp_traject(obs1),
-                      helper.comp_lp_traject(obs2)};
-  double d_lp_traject[]{helper.comp_d_lp_traject(obs1),
-                        helper.comp_d_lp_traject(obs2)};
+  double const lp_traject[]
+    { helper.comp_lp_traject(obs1), helper.comp_lp_traject(obs2) };
+  double const d_lp_traject[]
+    { helper.comp_d_lp_traject(obs1), helper.comp_d_lp_traject(obs2) };
 
   double out{std::log(d_lp_traject[0]) + std::log(d_lp_traject[1])};
 
@@ -109,7 +109,7 @@ double mmcif_log_Lik_both_obs
   arma::mat V{mat_no_alloc(2, 2 * n_causes, mem)};
   V.zeros();
   V(0, idx_cause_1) = 1;
-  V(0, idx_cause_2) = 1;
+  V(1, idx_cause_2) = 1;
 
   arma::mat vcov;
   helper.fill_vcov(vcov);
@@ -129,7 +129,7 @@ double mmcif_log_Lik_both_obs
   arma::mat vcov_cond_inv{mat_no_alloc(2 * n_causes, 2 * n_causes, mem)};
   arma::mat vcov_cond    {mat_no_alloc(2 * n_causes, 2 * n_causes, mem)};
   vcov_cond_inv = arma::inv_sympd(vcov);
-  vcov_cond_inv += V * V.t();
+  vcov_cond_inv += V.t() * V;
   vcov_cond = arma::inv_sympd(vcov_cond_inv);
 
   arma::mat vcov_cond_sub{mat_no_alloc(n_causes, n_causes, mem)};
@@ -161,7 +161,97 @@ double mmcif_log_Lik_one_obs
   (double const * __restrict__ par, param_indexer const &indexer,
    mmcif_data const &obs1, mmcif_data const &obs2,
    simple_mem_stack<double> &mem, ghq_data const &dat){
-  return 0; // TODO: implement
+  mmcif_comp_helper helper{indexer, par, mem};
+
+  double const lp_traject1{helper.comp_lp_traject(obs1)};
+  double const d_lp_traject1{helper.comp_d_lp_traject(obs1)};
+  auto const cause1 = obs1.cause;
+
+  double out{std::log(d_lp_traject1)};
+  out += helper.comp_trajector_cond_dens_obs_one(lp_traject1, cause1);
+
+  auto const n_causes = indexer.n_causes();
+  auto const cause = obs1.cause;
+  arma::mat logit_offsets{mat_no_alloc(n_causes, 2, mem)};
+  helper.fill_logit_offsets(logit_offsets.colptr(0), obs1);
+  helper.fill_logit_offsets(logit_offsets.colptr(1), obs2);
+
+  arma::mat vcov_cond;
+  helper.fill_cond_vcov_one_obs(vcov_cond, cause);
+
+  arma::vec cond_mean;
+  cond_mean = vcov_cond.col(cause + n_causes) * lp_traject1;
+
+  arma::mat const vcov_cond_sub
+    {vcov_cond.submat(0, 0, n_causes - 1, n_causes - 1)};
+  arma::vec const vcov_mean_sub{cond_mean.subvec(0, n_causes - 1)};
+
+  if(!obs2.has_finite_trajectory_prob){
+    auto mem_mark = mem.set_mark_raii();
+
+    arma::uvec which_cat{cause + 1, 0};
+    mixed_mult_logit_term<false> mult_term(logit_offsets, which_cat);
+    rescale_shift_problem<false> prob_use
+      (vcov_cond_sub, vcov_mean_sub, mult_term);
+    adaptive_problem prob(prob_use, mem, adaptive_rel_eps);
+
+    double integral_term{};
+    ghq(&integral_term, dat, prob, mem, ghq_target_size);
+    out += std::log(integral_term);
+    return out;
+  }
+
+  auto mem_mark = mem.set_mark_raii();
+  double integral_term{};
+  {
+    arma::mat logit_offset_obs1 = logit_offsets.col(0);
+    arma::uvec which_cat{cause + 1};
+
+    mixed_mult_logit_term<false> mult_term(logit_offset_obs1, which_cat);
+    rescale_shift_problem<false> prob_use
+      (vcov_cond_sub, vcov_mean_sub, mult_term);
+    adaptive_problem prob(prob_use, mem, adaptive_rel_eps);
+    ghq(&integral_term, dat, prob, mem, ghq_target_size);
+  }
+
+  arma::uvec which_cat{cause + 1, 0};
+  arma::vec rng_coefs, vcov_col;
+  for(size_t cause_2 = 0; cause_2 < n_causes; ++cause_2){
+    which_cat[1] = cause_2 + 1;
+
+    mixed_mult_logit_term<false> mult_term(logit_offsets, which_cat);
+
+    size_t const idx_traject{n_causes + cause_2};
+    vcov_col = vcov_cond.col(idx_traject).subvec(0, n_causes - 1);
+
+    rng_coefs = arma::solve
+      (vcov_cond_sub, vcov_col, arma::solve_opts::likely_sympd);
+    double const var_cond
+      {1 + vcov_cond(idx_traject, idx_traject)
+        - arma::dot(vcov_col, rng_coefs)};
+
+    double const lp_traject2{helper.comp_lp_traject(obs2, cause_2)};
+    double const shift_prob
+      {lp_traject2 - cond_mean[idx_traject]
+        + arma::dot(rng_coefs, vcov_mean_sub)};
+
+    rng_coefs *= -1;
+
+    mixed_probit_term<false> probit_term
+      (std::sqrt(var_cond), shift_prob, rng_coefs);
+    combined_problem comp_prob({&probit_term, &mult_term});
+
+    rescale_shift_problem<false> prob_use
+      (vcov_cond_sub, vcov_mean_sub, comp_prob);
+    adaptive_problem prob(prob_use, mem, adaptive_rel_eps);
+
+    double res{};
+    ghq(&res, dat, prob, mem, ghq_target_size);
+    integral_term -= res;
+  }
+
+  out += std::log(integral_term);
+  return out;
 }
 
 /// computes the log likelihood when both outcomes are censored
@@ -169,10 +259,138 @@ double mmcif_log_Lik_both_cens
   (double const * __restrict__ par, param_indexer const &indexer,
    mmcif_data const &obs1, mmcif_data const &obs2,
    simple_mem_stack<double> &mem, ghq_data const &dat){
-  return 0; // TODO: implement
+  if(obs2.has_finite_trajectory_prob && !obs1.has_finite_trajectory_prob)
+    return mmcif_log_Lik_both_cens(par, indexer, obs2, obs1, mem, dat);
+
+  mmcif_comp_helper helper{indexer, par, mem};
+  auto const n_causes = indexer.n_causes();
+
+  arma::mat logit_offsets{mat_no_alloc(n_causes, 2, mem)};
+  helper.fill_logit_offsets(logit_offsets.colptr(0), obs1);
+  helper.fill_logit_offsets(logit_offsets.colptr(1), obs2);
+
+  arma::mat vcov;
+  helper.fill_vcov(vcov);
+  arma::mat const vcov_sub = vcov.submat(0, 0, n_causes - 1, n_causes - 1);
+
+  if(!obs1.has_finite_trajectory_prob){
+    arma::uvec which_cat{0, 0};
+    auto mem_marker = mem.set_mark_raii();
+    mixed_mult_logit_term<false> mult_term(logit_offsets, which_cat);
+    rescale_problem<false> prob_use(vcov_sub, mult_term);
+    adaptive_problem prob(prob_use, mem, adaptive_rel_eps);
+
+    double integral{};
+    ghq(&integral, dat, prob, mem, ghq_target_size);
+
+    return std::log(integral);
+
+  }
+  else if(!obs2.has_finite_trajectory_prob){
+    double integral{};
+    auto mem_marker = mem.set_mark_raii();
+
+    {
+      arma::mat const logit_offsets_2 = logit_offsets.col(1);
+      arma::uvec const which_cat{0};
+      mixed_mult_logit_term<false> mult_term(logit_offsets_2, which_cat);
+      rescale_problem<false> prob_use(vcov_sub, mult_term);
+      adaptive_problem prob(prob_use, mem, adaptive_rel_eps);
+
+      ghq(&integral, dat, prob, mem, ghq_target_size);
+    }
+
+    arma::uvec which_cat{0, 0};
+    arma::vec rng_coefs, vcov_col;
+    for(size_t cause_1 = 0; cause_1 < n_causes; ++cause_1){
+      which_cat[0] = cause_1 + 1;
+
+      size_t const idx_traject{n_causes + cause_1};
+      vcov_col = vcov.col(idx_traject).subvec(0, n_causes - 1);
+
+      rng_coefs = arma::solve
+        (vcov_sub, vcov_col, arma::solve_opts::likely_sympd);
+      double const var_cond
+        {1 + vcov(idx_traject, idx_traject) - arma::dot(vcov_col, rng_coefs)};
+
+      double const lp_traject1{helper.comp_lp_traject(obs1, cause_1)};
+      rng_coefs *= -1;
+
+      mixed_probit_term<false> probit_term
+        (std::sqrt(var_cond), lp_traject1, rng_coefs);
+      mixed_mult_logit_term<false> mult_term(logit_offsets, which_cat);
+      combined_problem comp_prob({&probit_term, &mult_term});
+
+      rescale_problem<false> prob_use(vcov_sub, comp_prob);
+      adaptive_problem prob(prob_use, mem, adaptive_rel_eps);
+
+      double res{};
+      ghq(&res, dat, prob, mem, ghq_target_size);
+      integral -= res;
+    }
+
+    return std::log(integral);
+  }
+
+  auto comp_main_terms = [&](mmcif_data const &obs){
+    return std::exp(mmcif_log_Lik(par, indexer, obs, mem, dat)) - 1;
+  };
+
+  double integral{1};
+  auto mem_marker1 = mem.set_mark_raii();
+  integral += comp_main_terms(obs1);
+  integral += comp_main_terms(obs2);
+
+  // handle the interaction terms
+  arma::mat V{mat_no_alloc(2, n_causes, mem)};
+  arma::uvec which_cat(2);
+  arma::vec dens_point(vec_no_alloc(2, mem));
+
+  arma::mat pbvn_vcov{mat_no_alloc(2, 2, mem)};
+  arma::mat rng_coefs{mat_no_alloc(2, n_causes, mem)};
+
+  arma::mat const cond_mean_loadings =
+    arma::solve(vcov_sub,
+                vcov.submat(0, n_causes, n_causes - 1, 2 * n_causes - 1),
+                arma::solve_opts::likely_sympd).t();
+  arma::mat const vcov_rng_cond_traject =
+    vcov.submat(n_causes, n_causes, 2 * n_causes - 1, 2 * n_causes - 1)
+    - vcov.submat(n_causes, 0, 2 * n_causes - 1, n_causes - 1) *
+      cond_mean_loadings.t();
+
+  auto mem_marker2 = mem.set_mark_raii();
+
+  for(size_t cause_1 = 0; cause_1 < n_causes; ++cause_1){
+    which_cat[0] = cause_1 + 1;
+    dens_point[0] = -helper.comp_lp_traject(obs1, cause_1);
+
+    for(size_t cause_2 = 0; cause_2 < n_causes; ++cause_2){
+      which_cat[1] = cause_2 + 1;
+
+      V.zeros();
+      V(0, cause_1) = 1;
+      V(1, cause_2) = 1;
+      dens_point[1] = -helper.comp_lp_traject(obs2, cause_2);
+
+      pbvn_vcov = V * vcov_rng_cond_traject * V.t();
+      pbvn_vcov.diag() += 1;
+      rng_coefs = V * cond_mean_loadings;
+
+      cond_pbvn<false> pbvn_term(dens_point, pbvn_vcov, rng_coefs);
+      mixed_mult_logit_term<false> mult_term(logit_offsets, which_cat);
+      combined_problem comp_prob({&pbvn_term, &mult_term});
+
+      rescale_problem<false> prob_use(vcov_sub, comp_prob);
+      adaptive_problem prob(prob_use, mem, adaptive_rel_eps);
+
+      double res{};
+      ghq(&res, dat, prob, mem, ghq_target_size);
+      integral += res;
+    }
+  }
+
+  return std::log(integral);
 }
-
-
 
 } // namespace
 
--- src/test-mmcif-logLik.cpp
+++ src/test-mmcif-logLik.cpp
@@ -9,8 +9,8 @@ const ghqCpp::ghq_data ghq_dat_use{ghq_nodes, ghq_weights, 10};
 
 } // namespace
 
-context("mmcif_log_Lik works as expected") {
-  test_that("mmcif_log_Lik works with singleton data") {
+context("mmcif_log_Lik works as expected with singleton data") {
+  test_that("mmcif_log_Lik works") {
     /*
      set.seed(321)
      n_cov_traject <- 4L
@@ -55,7 +55,7 @@ context("mmcif_log_Lik works as expected") {
      cbind(d_g(ti), matrix(0., length(ti), length(covs_traject)))
 
 # the matrices we will need
-     dput(covs_traject_w_tine <- g_w_covs(obs_time) |> drop())
+     dput(covs_traject_w_time <- g_w_covs(obs_time) |> drop())
      dput(d_covs_traject_w_time <- d_g_w_covs(obs_time) |> drop())
 
      dput(gl <- fastGHQuad::gaussHermiteData(10L))
@@ -76,7 +76,7 @@ context("mmcif_log_Lik works as expected") {
      idx_cause <- cause + n_causes
 
      res <- log(-d_covs_traject_w_time %*% coefs_traject[, cause]) |> drop()
-     res <- res + dnorm(-covs_traject_w_tine %*% coefs_traject[, cause] |> drop(),
+     res <- res + dnorm(-covs_traject_w_time %*% coefs_traject[, cause] |> drop(),
      sd = sqrt(1 + vcov[idx_cause, idx_cause]), log = TRUE)
 
      M <- solve(vcov)
@@ -84,7 +84,7 @@ context("mmcif_log_Lik works as expected") {
      M <- solve(M)
 
      mean_cond <- M[, idx_cause] *
-     sum(-covs_traject_w_tine %*% coefs_traject[, cause])
+     sum(-covs_traject_w_time %*% coefs_traject[, cause])
      etas <- covs_risk %*% coefs_risk |> drop()
      etas <- etas + mean_cond[1:n_causes]
 
@@ -107,7 +107,7 @@ context("mmcif_log_Lik works as expected") {
      s <- sqrt(1 + vcov[idx_cause, idx_cause] -
      sum(vcov[idx_cause, 1:n_causes] * (-z)))
 
-     offset <- -covs_traject_w_tine %*% coefs_traject[, cause] |> drop()
+     offset <- -covs_traject_w_time %*% coefs_traject[, cause] |> drop()
 
      integrand <- integrand - mixed_mult_logit_n_probit_term(
      eta = as.matrix(etas),
@@ -149,5 +149,369 @@ context("mmcif_log_Lik works as expected") {
     double const res{mmcif_log_Lik(par, indexer, dat, mem, ghq_dat_use)};
     constexpr double truth{-1.04608338904514};
     expect_true(std::abs(res - truth) < std::abs(truth) * 1e-8);
+  }
+}
+
+namespace {
+/*
+ set.seed(321)
+ n_cov_traject <- 4L
+ n_cov_risk <- 2L
+ n_causes <- 3L
+
+ g <- \(x) cbind(x, log(x))
+ d_g <- \(x) cbind(1, 1/x)
+
+ vcov <-
+ drop(rWishart(1, 2 * n_causes, diag(1/2/n_causes, 2 * n_causes))) |>
+ drop() |> round(3)
+
+ coefs_risk <- runif(n_causes * n_cov_risk, -1) |>
+ round(3) |> matrix(n_cov_risk)
+ coefs_traject <- runif(n_causes * n_cov_traject, -1) |>
+ round(3) |> matrix(n_cov_traject)
+ coefs_traject <- rbind(-.5, -.25, coefs_traject)
+ c(coefs_risk, coefs_traject, vcov) |> dput()
+
+# sample a plausible value
+ u <- mvtnorm::rmvnorm(1, sigma = vcov) |> drop()
+
+ g_w_covs <- \(ti, covs_traject)
+ cbind(g(ti), rep(covs_traject, each = length(ti)) |> matrix(length(ti)))
+ d_g_w_covs <- \(ti, covs_traject)
+ cbind(d_g(ti), matrix(0., length(ti), length(covs_traject)))
+
+ sample_obs <- \(){
+ covs_risk <- runif(n_cov_risk, -1) |> round(3)
+ covs_traject <- runif(n_cov_traject, -1) |> round(3)
+
+ etas <- covs_risk %*% coefs_risk |> drop()
+ etas <- etas + u[1:n_causes]
+ phats <- c(exp(etas), 1) / (1 + sum(exp(etas)))
+ cause <- sample(n_causes + 1, 1, prob = phats)
+ stopifnot(cause < n_causes + 1) # we did not implement this case
+
+ rng <- runif(1)
+ obs_time <- uniroot(
+ \(ti){
+ lp <- -g_w_covs(ti, covs_traject) %*% coefs_traject[, cause] -
+ u[cause + n_causes]
+ pnorm(lp) - rng
+ },
+ c(1e-16, 1000))$root
+
+ list(covs_risk = covs_risk,
+ covs_traject_w_time = g_w_covs(obs_time, covs_traject) |> drop(),
+ d_covs_traject_w_time = d_g_w_covs(obs_time, covs_traject) |> drop(),
+ cause = cause)
+ }
+
+ dput(obs <- replicate(2, sample_obs(), simplify = FALSE))
+
+ dput(gl <- fastGHQuad::gaussHermiteData(10L))
+ */
+
+constexpr size_t n_cov_risk{2},
+                 n_cov_traject{6},
+                 n_causes{3},
+                 cause[]{0, 2};
+
+param_indexer const indexer{n_cov_risk, n_cov_traject, n_causes};
+
+constexpr double covs_risk1[]{-0.605, -0.876},
+                 covs_risk2[]{0.584, 0.576},
+              covs_traject1[]{0.85860256800553, -0.152449132274265, -0.734, 0.062, 0.957, 0.198},
+              covs_traject2[]{4.78877056139744, 1.5662737107113, 0.304, 0.267, -0.669, 0.204},
+            d_covs_traject1[]{1, 1.16468321580138, 0, 0, 0, 0},
+            d_covs_traject2[]{1, 0.208821865065129, 0, 0, 0, 0},
+                        par[]{-0.647, 0.076, -0.439, -0.98, -0.806, 0.324, -0.5, -0.25, 0.355, -0.737, -0.475, -0.365, -0.5, -0.25, 0.353, -0.874, -0.871, 0.322, -0.5, -0.25, -0.791, 0.474, -0.85, 0.782, 1.974, -0.235, 0.154, 0.195, 0.108, 0.059, -0.235, 0.401, 0.163, -0.161, 0.597, -0.155, 0.154, 0.163, 0.4, 0.026, 0.047, 0.179, 0.195, -0.161, 0.026, 0.506, -0.493, 0.018, 0.108, 0.597, 0.047, -0.493, 2.031, 0.052, 0.059, -0.155, 0.179, 0.018, 0.052, 0.578};
+} // namespace
+
+context("mmcif_log_Lik works as expected with bivariate data") {
+  test_that("mmcif_log_Lik works when both individuals are observed") {
+    /*
+     library(ghqCpp)
+     local({
+     etas <- sapply(obs, \(x) x$covs_risk %*% coefs_risk)
+     covs_traject <- sapply(
+     obs,
+     \(x, cause) -x$covs_traject_w_time %*% coefs_traject[, x$cause])
+     d_covs_traject <- sapply(
+     obs,
+     \(x) -x$d_covs_traject_w_time %*% coefs_traject[, x$cause])
+
+     V <- matrix(0, 2, 2 * n_causes)
+     V[1, obs[[1]]$cause + n_causes] <- 1
+     V[2, obs[[2]]$cause + n_causes] <- 1
+     out <- sum(log(d_covs_traject)) +
+     mvtnorm::dmvnorm(
+     covs_traject, sigma = diag(2) + V %*% tcrossprod(vcov, V), log = TRUE)
+
+     vcov_cond <- solve(crossprod(V) + solve(vcov))
+     mean_cond <- vcov_cond %*% crossprod(V, covs_traject) |> drop()
+
+     integral <- mixed_mult_logit_term(
+     eta = etas + mean_cond[1:n_causes],
+     Sigma = vcov_cond[1:n_causes, 1:n_causes],
+     which_category = sapply(obs, `[[`, "cause"),
+     weights = gl$w, nodes = gl$x)
+
+     dput(out + log(integral))
+     })
+     */
+    ghqCpp::simple_mem_stack<double> mem;
+
+    mmcif_data
+      obs1{covs_traject1, d_covs_traject1, covs_risk1, true, cause[0]},
+      obs2{covs_traject2, d_covs_traject2, covs_risk2, true, cause[1]};
+
+    double const res{mmcif_log_Lik(par, indexer, obs1, obs2, mem, ghq_dat_use)};
+    constexpr double truth{-7.77301433778719};
+    expect_true(std::abs(res - truth) < std::abs(truth) * 1e-8);
+  }
+
+  test_that("mmcif_log_Lik works when one individual is observed and one is censored") {
+    /*
+     library(ghqCpp)
+     local({
+     etas <- sapply(obs, \(x) x$covs_risk %*% coefs_risk)
+     covs_traject <- sapply(
+     obs,
+     \(x, cause) -x$covs_traject_w_time %*% coefs_traject[, x$cause])
+     d_covs_traject <- sapply(
+     obs,
+     \(x) -x$d_covs_traject_w_time %*% coefs_traject[, x$cause])
+
+     v <- numeric(2 * n_causes)
+     v[n_causes + obs[[1]]$cause] <- 1
+     out <- log(d_covs_traject[1]) + dnorm(
+     covs_traject[1], sd = sqrt(1 + v %*% vcov %*% v), log = TRUE)
+
+     vcov_cond <- solve(tcrossprod(v) + solve(vcov))
+     mean_cond <- drop(vcov_cond %*% v) * covs_traject[1]
+
+     integral <- mixed_mult_logit_term(
+     eta = etas + mean_cond[1:n_causes],
+     Sigma = vcov_cond[1:n_causes, 1:n_causes],
+     which_category = c(obs[[1]]$cause, 0L),
+     weights = gl$w, nodes = gl$x)
+
+     dput(out + log(integral))
+     })
+     */
+
+    ghqCpp::simple_mem_stack<double> mem;
+
+    {
+      mmcif_data
+        obs1{covs_traject1, d_covs_traject1, covs_risk1, true, cause[0]},
+        obs2{covs_traject2, d_covs_traject2, covs_risk2, false, n_causes};
+
+      double const res
+        {mmcif_log_Lik(par, indexer, obs1, obs2, mem, ghq_dat_use)};
+      constexpr double truth{-4.5240025456571};
+      expect_true(std::abs(res - truth) < std::abs(truth) * 1e-8);
+    }
+
+    /*
+     local({
+     etas <- sapply(obs, \(x) x$covs_risk %*% coefs_risk)
+     covs_traject <- sapply(
+     obs,
+     \(x, cause) -x$covs_traject_w_time %*% coefs_traject[, x$cause])
+     d_covs_traject <- sapply(
+     obs,
+     \(x) -x$d_covs_traject_w_time %*% coefs_traject[, x$cause])
+
+     v <- numeric(2 * n_causes)
+     v[n_causes + obs[[1]]$cause] <- 1
+     out <- log(d_covs_traject[1]) + dnorm(
+     covs_traject[1], sd = sqrt(1 + v %*% vcov %*% v), log = TRUE)
+
+     vcov_cond <- solve(tcrossprod(v) + solve(vcov))
+     mean_cond <- drop(vcov_cond %*% v) * covs_traject[1]
+
+     integral <- mixed_mult_logit_term(
+     eta = as.matrix(etas[, 1]) + mean_cond[1:n_causes],
+     Sigma = vcov_cond[1:n_causes, 1:n_causes],
+     which_category = obs[[1]]$cause, weights = gl$w, nodes = gl$x)
+
+     integrals <- sapply(1:n_causes, \(cause){
+     idx_cause <- cause + n_causes
+     rng_coefs <- solve(vcov_cond[1:n_causes, 1:n_causes],
+     vcov_cond[1:n_causes, idx_cause])
+
+     s <- sqrt(1 + vcov_cond[idx_cause, idx_cause] -
+     vcov_cond[idx_cause, 1:n_causes] %*% rng_coefs)
+     shift <- -obs[[2]]$covs_traject_w_time %*% coefs_traject[, cause] -
+     mean_cond[idx_cause]
+
+     rng_coefs <- -rng_coefs
+
+     mixed_mult_logit_n_probit_term(
+     eta = etas + mean_cond[1:n_causes],
+     which_category = c(obs[[1]]$cause, cause), s = s, eta_probit = shift,
+     Sigma = vcov_cond[1:n_causes, 1:n_causes], z = rng_coefs,
+     weights = gl$w, nodes = gl$x)
+     })
+     integral <- integral - sum(integrals)
+
+     dput(out + log(integral))
+     })
+     */
+
+    {
+      mmcif_data
+        obs1{covs_traject1, d_covs_traject1, covs_risk1, true, cause[0]},
+        obs2{covs_traject2, d_covs_traject2, covs_risk2, true, n_causes};
+
+      constexpr double truth{-4.38551973954109};
+      {
+        double const res
+          {mmcif_log_Lik(par, indexer, obs1, obs2, mem, ghq_dat_use)};
+        expect_true(std::abs(res - truth) < std::abs(truth) * 1e-8);
+      }
+
+      double const res
+        {mmcif_log_Lik(par, indexer, obs2, obs1, mem, ghq_dat_use)};
+      expect_true(std::abs(res - truth) < std::abs(truth) * 1e-8);
+    }
+  }
+
+  test_that("mmcif_log_Lik works when both individuals are censored") {
+    /*
+     local({
+     etas <- sapply(obs, \(x) x$covs_risk %*% coefs_risk)
+     mixed_mult_logit_term(
+     eta = etas, Sigma = vcov[1:n_causes, 1:n_causes],
+     which_category = integer(2), weights = gl$w, nodes = gl$x) |>
+     log() |> dput()
+     })
+     */
+
+    ghqCpp::simple_mem_stack<double> mem;
+
+    {
+      mmcif_data
+        obs1{covs_traject1, d_covs_traject1, covs_risk1, false, n_causes},
+        obs2{covs_traject2, d_covs_traject2, covs_risk2, false, n_causes};
+
+      double const res
+        {mmcif_log_Lik(par, indexer, obs1, obs2, mem, ghq_dat_use)};
+      constexpr double truth{-3.01479043790748};
+      expect_true(std::abs(res - truth) < std::abs(truth) * 1e-8);
+    }
+
+    /*
+     local({
+     etas <- sapply(obs, \(x) x$covs_risk %*% coefs_risk)
+
+     integral <- mixed_mult_logit_term(
+     eta = as.matrix(etas[, 2]), Sigma = vcov[1:n_causes, 1:n_causes],
+     which_category = 0L, weights = gl$w, nodes = gl$x)
+
+     integrals <- sapply(1:n_causes, \(cause){
+     lp_traject <- -obs[[1]]$covs_traject_w_time %*% coefs_traject[, cause] |>
+     drop()
+
+     idx_cause <- cause + n_causes
+     rng_coefs <-
+     solve(vcov[1:n_causes, 1:n_causes], vcov[1:n_causes, idx_cause])
+
+     s <- sqrt(1 + vcov[idx_cause, idx_cause] -
+     vcov[idx_cause, 1:n_causes] %*% rng_coefs)
+
+     rng_coefs <- -rng_coefs
+
+     mixed_mult_logit_n_probit_term(
+     eta = etas, which_category = c(cause, 0L), s = s,
+     eta_probit = lp_traject, Sigma = vcov[1:n_causes, 1:n_causes],
+     z = rng_coefs, weights = gl$w, nodes = gl$x)
+     })
+
+     log(integral - sum(integrals)) |> dput()
+     })
+     */
+
+    {
+      mmcif_data
+        obs1{covs_traject1, d_covs_traject1, covs_risk1, true, n_causes},
+        obs2{covs_traject2, d_covs_traject2, covs_risk2, false, n_causes};
+
+      constexpr double truth{-2.18417107496657};
+      {
+        double const res
+          {mmcif_log_Lik(par, indexer, obs1, obs2, mem, ghq_dat_use)};
+        expect_true(std::abs(res - truth) < std::abs(truth) * 1e-8);
+      }
+
+      double const res
+        {mmcif_log_Lik(par, indexer, obs2, obs1, mem, ghq_dat_use)};
+      expect_true(std::abs(res - truth) < std::abs(truth) * 1e-8);
+    }
+
+    /*
+     library(ghqCpp)
+     local({
+     vcov_sub <- vcov[1:n_causes, 1:n_causes]
+
+     integrals_substract <- sapply(obs, \(obs_i){
+     sapply(1:n_causes, \(cause){
+     etas <- obs_i$covs_risk %*% coefs_risk |> drop()
+
+     idx_cause <- cause + n_causes
+     z <- -solve(vcov_sub, vcov[1:n_causes, idx_cause]) |> drop()
+     s <- sqrt(1 + vcov[idx_cause, idx_cause] -
+     sum(vcov[idx_cause, 1:n_causes] * (-z)))
+
+     offset <- -obs_i$covs_traject_w_time %*% coefs_traject[, cause] |>
+     drop()
+
+     mixed_mult_logit_n_probit_term(
+     eta = as.matrix(etas),
+     which_category = cause, s = s, eta_probit = offset,
+     Sigma = vcov_sub, z = z, weights = gl$w, nodes = gl$x)
+     })
+     })
+
+     etas <- sapply(obs, \(x) x$covs_risk %*% coefs_risk)
+     rng_coefs <- solve(vcov_sub, vcov[1:n_causes, -(1:n_causes)]) |> t()
+     vcov_cond <- vcov[1:n_causes + n_causes, 1:n_causes + n_causes] -
+     vcov[1:n_causes + n_causes, 1:n_causes] %*% t(rng_coefs)
+
+     integrals_adds <- sapply(
+     1:n_causes,
+     \(cause1) sapply(
+     1:n_causes, \(cause2){
+     V <- matrix(0, 2, n_causes)
+     V[1, cause1] <- 1
+     V[2, cause2] <- 1
+
+     shift <- -c(obs[[1]]$covs_traject_w_time %*% coefs_traject[, cause1],
+     obs[[2]]$covs_traject_w_time %*% coefs_traject[, cause2])
+
+     rng_coefs <- -V %*% rng_coefs
+     vcov_cond_pbvn <- diag(2) + V %*% tcrossprod(vcov_cond, V)
+
+     mixed_mult_logit_n_cond_pbvn(
+     eta = etas, which_category = c(cause1, cause2),
+     eta_pbvn = -shift, Psi = vcov_cond_pbvn, V = -rng_coefs,
+     Sigma = vcov_sub, weights = gl$w, nodes = gl$x)
+     }))
+
+     log(1 - sum(integrals_substract) + sum(integrals_adds)) |> dput()
+     })
+     */
+
+    {
+      mmcif_data
+        obs1{covs_traject1, d_covs_traject1, covs_risk1, true, n_causes},
+        obs2{covs_traject2, d_covs_traject2, covs_risk2, true, n_causes};
+
+      double const res
+        {mmcif_log_Lik(par, indexer, obs1, obs2, mem, ghq_dat_use)};
+      constexpr double truth{-2.00247380641032};
+      expect_true(std::abs(res - truth) < std::abs(truth) * 1e-8);
+    }
   }
 }
