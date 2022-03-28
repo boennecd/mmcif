4b497f34f81ecaa74adc539007961cc0b2f62551
Benjamin Christoffersen <boennecd@gmail.com>
2022-03-28 14:21:50 +0200

    add vignettes to the package

---
+++ vignettes/create-vignette.R
@@ -0,0 +1,18 @@
+library(rmarkdown)
+(function(){
+  setwd("vignettes")
+
+  render("mmcif.Rmd", output_format = "html_document")
+  render("mmcif.Rmd", output_format = github_document(
+    pandoc_args = "--webtex=https://render.githubusercontent.com/render/math?math="),
+    output_file = "README.md")
+  file.copy("README.md", file.path("..", "README.md"), overwrite = TRUE)
+  unlink("README.md")
+  unlink("README.html")
+
+  fs <- list.files(file.path("man", "figures"))
+  for(f in fs)
+    file.copy(file.path("man", "figures", f),
+              file.path("..", "man", "figures", f), overwrite = TRUE)
+  unlink("man", recursive = TRUE)
+})()
